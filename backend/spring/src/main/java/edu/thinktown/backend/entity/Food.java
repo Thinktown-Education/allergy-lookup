0b6efc9c66415cf1b870fe5136bb2053f795922f
nkarl <21993921+nkarl@users.noreply.github.com>
2023-06-28 11:21:00 -0700

    Added Food Entity.

---
+++ backend/spring/src/main/java/edu/thinktown/backend/entity/Food.java
@@ -0,0 +1,17 @@
+package edu.thinktown.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity(name = "food")
+@Data
+public class Food {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    String foodName;
+    String brand;
+}
